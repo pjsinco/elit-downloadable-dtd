8e4a3dc85e6bc5cf33245f092be05152a3e0c115
pjsinco <psinco@gmail.com>
2017-03-30 08:48:04 -0500

    Bump version to 1.0.2

--- elit-downloadable.php
+++ elit-downloadable.php
@@ -3,7 +3,7 @@
 Plugin Name: Elit Downloadable
 Plugin URI:  
 Description: Make images and other assets downloadable
-Version:  1.0.1
+Version:  1.0.2
 Author: Patrick Sinco
 Author URI: github.com/pjsinco
 License: GPL2
